637f882432ff74e7384fca3ec0eb473cd848c1ab
Mike <mido3801@colorado.edu>
2023-09-16 15:56:35 -0400

    stuff

--- scripts.js
+++ scripts.js
@@ -6,3 +6,21 @@ function toggleContent(buttonElement) {
         contentElement.style.display = "none";
     }
 }
+
+
+// Function to show a specific month and hide others
+function showMonth(monthId) {
+    // Get all month elements
+    const months = document.querySelectorAll('.month');
+
+    // Hide all months
+    months.forEach(month => month.style.display = 'none');
+
+    // Show the selected month
+    document.getElementById(monthId).style.display = 'block';
+}
+
+// Show January by default
+window.onload = function() {
+    showMonth('january');
+};
